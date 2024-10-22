f815b7e034c0e939e0ba9d8d0adf8f82ce5a432c
johnnathabessa <johnnathabessa@gmaiil.com>
2024-10-21 21:45:13 -0300

    adicionado modal

--- src/app/park/page.js
+++ src/app/park/page.js
@@ -1,9 +1,37 @@
+"use client"
 import SideBar from "@/components/SideBar/SideBar";
+import Button from "@/components/Button/Button";
+import { Trash } from "@phosphor-icons/react/dist/ssr";
+import ModalWithBackdrop from "@/components/ModalWithBackdrop/ModalwithBackdrop";
+import { useState } from "react";
 
 export default function ParkPage() {
+    const [showModal, setShowModal] = useState(false)
     return (
         <div className="flex relative min-h-full items-center flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <SideBar />
+            <ModalWithBackdrop show={showModal} onClose={() => setShowModal(false)}></ModalWithBackdrop>
+            <div className="flex flex-col flex-grow w-full justify-start items-end pl-24">
+                <div className="flex">
+                    <Button onClick={() => setShowModal(true)}> Adicionar Vaga </Button>
+                </div>
+                <div className="flex flex-col space-y-3 w-full mt-6">
+                <div className=" flex w-full justify-between bg-gray-200 rounded-xl p-4">
+                    <div className="flex">
+                        VAGA 001
+                    </div>
+                    <div className="flex cursor-pointer">
+                        <Trash size={20} />
+
+                    </div>
+
+                </div>
+
+            </div>
+            </div>
+           
         </div>
     )
+    
 }
+
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button ({text}) {
+export default function Button ({children,onClick=() => {}}) {
     return (
         <div>
-              <button type="submit" className="block w-full py-2 text-center font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
-                {text}
+              <button type="submit" onClick={onClick} className="block w-full p-2 text-center font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                {children}
               </button>
           </div>
     )
